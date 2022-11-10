48f2db360b2164a2c12eff41cf192c7c8f59b94c
architjain11 <architjain@gmail.com>
2022-11-10 12:36:28 +0530

    commented debug code removed

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from flask import Flask, render_template, request
 
+# ML Model prediction
 data = pd.read_csv("static/RealEstateDataset.csv")
 
 x=data.drop(['Y house price of unit area', 'X5 latitude', 'X6 longitude', 'X1 transaction date', 'No'], axis=1)
@@ -17,32 +18,8 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_
 model = LinearRegression()
 model.fit(x, y)
 accuracy = model.score(x_test, y_test)
-# print(accuracy)
-# y_pred = model.predict(x_test)
-# op = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
-# print(op)
-# print(x)
 
-# age=2
-# metro=3
-# store=3
-# new_data = pd.DataFrame({'X2 house age': [age], 
-#                     'X3 distance to the nearest MRT station': [metro], 
-#                     'X4 number of convenience stores': [store]})
-# price=model.predict(new_data)
-# print(price[0])
-# # data=list()
-# # data.append(['X2 house age', 'X3 distance to the nearest MRT station', 'X4 number of convenience stores'])
-# # data.append([age, metro, store])
-# # data = np.array(data)
-# data = np.expand_dims(data, 0)
-# price = model.predict(data)
-# print(price)
-
-
-
-
-
+# Flask application begins
 app = Flask(__name__)
 
 @app.route("/")
